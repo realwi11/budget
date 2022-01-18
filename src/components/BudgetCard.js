2acc60acb952bbd1ef18189f327e57385e95f7a1
GitWill <stidhemw@gmail.com>
2022-01-18 12:18:13 -0800

    dumb change

--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -4,7 +4,7 @@ import { currencyFormatter } from "../utils"
 export default function BudgetCard( {name,amount,max}  ) {
     return (
         <Card>
-            <Card.Body>
+            <Card.Body> 
                 <Card.Title className="d-flex justify-content-between align-items-baseline fw-normal mb-3">
                     <div className="me-2">{name}</div>
                     <div className="d-flex align-items-baseline">
